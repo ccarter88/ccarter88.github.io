8fe783be3805fc15f38701bca98cdb566f9fdd94
ccarter88 <carterchloe599@gmail.com>
2026-09-03 12:49:23 +0000

    updating colors

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -17,32 +17,48 @@ $(function () {
     }
 
     // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
-    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+    createPlatform(-50, -50, canvas.width + 100, 50, "#0d0d0d"); // top wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "#0d0d0d"); // bottom wall
+    createPlatform(-50, -50, 50, canvas.height + 500, "#0d0d0d"); // left wall
+    createPlatform(canvas.width, -50, 50, canvas.height + 100, "#0d0d0d"); // right wall
 
     //////////////////////////////////
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
+createPlatform(100, 630, 150, 20, "#be6ea4");
+createPlatform(345, 500, 150, 20, "#be6ea4");
 
+createPlatform(510, 590, 150, 20, "#be6ea4");
 
+createPlatform(685,455, 150, 20, "#be6ea4");
+
+createPlatform(855,635, 150, 20, "#be6ea4");
+
+createPlatform(1055, 500,150, 20, "#be6ea4");
 
 
     // TODO 3 - Create Collectables
+createCollectable("guccimorty", 400, 450);
 
 
+createCollectable("guccimorty", 685, 400);
+createCollectable("guccimorty", 525, 10, 1.0, 0.0);
 
+createCollectable("guccimorty",1085,460);
+createCollectable("guccimorty", 850, 10, 1.0, 0.0);
     
     // TODO 4 - Create Cannons
+createCannon("top", 200, 600);
+createCannon("right", 750, 600);
 
-
+createCannon("top", 700, 980);
+createCannon("left", 650, 1000);
     
     
     //////////////////////////////////
--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -102,4 +102,6 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  guccimorty: { image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcI5uD-J5TmNc_e_K0zlcUhWbIvbvfU5JBuCzgWiOgfQ&s=10"}
+  unicorn: {}
 };
